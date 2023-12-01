aa84aee87098a757a165fa20f6d5009f5097a4b3
SystemCallBeam <tkhaeamadan@gmail.com>
2023-12-01 16:11:43 +0700

    create 10 dot with random color

--- MyPicture.java
+++ MyPicture.java
@@ -1,12 +1,20 @@
 import java.awt.*;
+import java.util.Random;
+
 import javax.swing.*;
 
 public class MyPicture extends JPanel {
     public static final int WIDTH = 600, HEIGHT = 600;
+
+    public static Random rand = new Random();
+    public static Color randomColor[] = new Color[10];
+
     public static void main(String[] args) {
-        // create frame?
         MyPicture g = new MyPicture();
         JFrame f = new JFrame("My Picture");
+        for (int i = 0; i < 10; i++) {
+            randomColor[i] = new Color(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256));
+        }
         f.setSize(WIDTH, HEIGHT);
         f.add(g);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -14,30 +22,20 @@ public class MyPicture extends JPanel {
     }
 
     public void paintComponent(Graphics g) {
-        // plot(g, 2, 2);?
-        int x[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-        int y[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-        draw(g, 100, 10, 100, 200);
-    }
+        int x[] = { 590, 250, 1, 450, 299, 299, 448, 48, 150, 598 };
+        int y[] = { 200, 250, 1, 150, 299, 450, 448, 330, 510, 598 };
+        for (int i = 0; i < 10; i++) {
 
-    public void plot(Graphics g, int x, int y) {
-        int s = 1;
-        g.fillRect(x, y, s, s);
-    }
-
-    public void draw(Graphics g, int x0, int y0, int x1, int y1) {
-        for (int i = 0; i < HEIGHT; i++) {
-            for (int j = 0; j < WIDTH; j++) {
-                double a = distance(x0, y0, j, i), b = distance(j, i, x1, y1),
-                c = distance(x0, y0, x1, y1);
-                if (Math.abs(a + b - c) < 1) {
-                    plot(g, j, i);
-                }
-            }
+            g.setColor(randomColor[i]);
+            plot(g, x[i], y[i]);
         }
     }
 
-    public double distance(int x0, int y0, int x1, int y1) {
-        return Math.sqrt((Math.pow(x1-x0, 2))+(Math.pow(y1-y0, 2)));
+    public void plot(Graphics g, int x, int y) {
+        plot(g, x, y, 8);
+    }
+
+    public void plot(Graphics g, int x, int y, int s) {
+        g.fillRect(x, y, s, s);
     }
 }
