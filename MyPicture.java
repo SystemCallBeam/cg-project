786d13220e808ba7b154f0de1695bcecd2f0c922
SystemCallBeam <tkhaeamadan@gmail.com>
2023-12-11 13:05:13 +0700

    "add bresenham algorithm"

--- MyPicture.java
+++ MyPicture.java
@@ -25,12 +25,15 @@ public class MyPicture extends JPanel {
     public void paintComponent(Graphics g) {
         int x[] = { 590, 250, 1, 450, 299, 299, 448, 48, 150, 598 };
         int y[] = { 200, 250, 1, 150, 299, 450, 448, 330, 510, 598 };
-        for (int i = 0; i < 10; i++) {
+        /* for (int i = 0; i < 10; i++) {
 
             g.setColor(randomColor[i]);
             plot(g, x[i], y[i]);
-        }
-        curve(g, FRAMEBITS, ERROR, WIDTH, HEIGHT, ALLBITS, ABORT);
+            line(g, x[i], y[i], x[(i + 1) % 10], y[(i + 1) % 10]);
+        } */
+        line(g, 100, 100, 400, 200);
+        line(g, 400, 300, 100, 200);
+        line(g, 100, 100, 200, 400);
     }
 
     public void curve(Graphics g, int x, int y, int width, int height, int sttang, int arcang) {
@@ -43,16 +46,13 @@ public class MyPicture extends JPanel {
     }
 
     public void line(Graphics g, int x1, int y1, int x2, int y2) {
-        g.drawLine(x1, y1, x2, y2);
-        // drawLine(g, x1, y1, x2, y2);
-    }
-
-    public void drawLine(Graphics g, int x1, int y1, int x2, int y2) {
-        plot(g, x2, y2);
+        // g.drawLine(x1, y1, x2, y2);
+        // dda(g, x1, y1, x2, y2);
+        bresenham(g, x1, y1, x2, y2);
     }
 
     public void plot(Graphics g, int x, int y) {
-        plot(g, x, y, 8);
+        plot(g, x, y, 1);
     }
 
     public void plot(Graphics g, int x, int y, int s) {
@@ -63,15 +63,53 @@ public class MyPicture extends JPanel {
         int dx = x2 - x1;
         int dy = y2 - y1;
         int step = Math.max(Math.abs(dx), Math.abs(dy));
-        int x = x1;
-        int y = y1;
-        int xinc = dx/step;
-        int yinc = dy/step;
+        float x = x1;
+        float y = y1;
+        float xinc = dx / (float) step;
+        float yinc = dy / (float) step;
 
         for (int i = 0; i < step; i++) {
-            plot(g, x, y);
+            plot(g, (int) x, (int) y);
             x += xinc;
             y += yinc;
+        }
+    }
+
+    public void bresenham(Graphics g, int x1, int y1, int x2, int y2) {
+        int dx = Math.abs(x2 - x1);
+        int dy = Math.abs(y2 - y1);
+
+        int sx = (x1 < x2) ? 1 : -1;
+        int sy = (y1 < y2) ? 1 : -1;
+        boolean isSwap = false;
+
+        if (dy > dx) {
+            int tmp = dx;
+            dx = dy;
+            dy = tmp;
+            isSwap = true;
+        }
+        int d = 2 * dy - dx;
+        int x = x1;
+        int y = y1;
+
+        for (int i = 0; i < dx; i++) {
+            plot(g, x, y);
+
+            if (d >= 0) {
+                if (isSwap)
+                    x += sx;
+                else
+                    y += sy;
+
+                d -= 2 * dx;
+            }
+            if (isSwap)
+                y += sy;
+            else
+                x += sx;
+
+            d += 2 * dy;
         }
     }
 }
