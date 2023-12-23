57f53705fd453b8161130dd336334daf2f84f6d6
SystemCallBeam <tkhaeamadan@gmail.com>
2023-12-23 12:56:57 +0700

    edit floodfill

--- MyPicture.java
+++ MyPicture.java
@@ -1,4 +1,7 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
 
 import javax.swing.*;
@@ -23,22 +26,81 @@ public class MyPicture extends JPanel {
 
     // (Fill, Draw) (Rect, Oval, Line, Arc)
     public void paintComponent(Graphics g) {
+        BufferedImage buffer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = buffer.createGraphics();
+
         int x[] = { 590, 250, 1, 450, 299, 299, 448, 48, 150, 598 };
         int y[] = { 200, 250, 1, 150, 299, 450, 448, 330, 510, 598 };
-        /* for (int i = 0; i < 10; i++) {
 
-            g.setColor(randomColor[i]);
-            plot(g, x[i], y[i]);
-            line(g, x[i], y[i], x[(i + 1) % 10], y[(i + 1) % 10]);
-        } */
-        line(g, 100, 100, 400, 200);
-        line(g, 400, 300, 100, 200);
-        line(g, 100, 100, 200, 400);
+        for (int i = 0; i < 10; i++) {
+
+            g2.setColor(randomColor[i]);
+            // plot(g, x[i], y[i]);
+            // line(g, x[i], y[i], x[(i + 1) % 10], y[(i + 1) % 10]);
+            bezier(g2, x[i], y[i], x[(i + 1) % 10], y[(i + 1) % 10], x[(i + 2) % 10],
+                    y[(i + 2) % 10], x[(i + 3) % 10], y[(i + 3) % 10]);
+        }
+
+        buffer = floodFill(buffer, 100, 50, Color.white, Color.red);
+        buffer = floodFill(buffer, 100, 100, Color.white, Color.blue);
+
+        g.drawImage(buffer, 0, 0, null);
+
     }
 
     public void curve(Graphics g, int x, int y, int width, int height, int sttang, int arcang) {
         g.drawArc(x, y, width, height, sttang, arcang);
         // drawArc(g, x, y, width, height, sttang, arcang)
+    }
+
+    public void bezier(Graphics g, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
+        float n = 2000;
+        float t = 0;
+        for (; t < 1; t += (1 / n)) {
+            double x = (Math.pow(1 - t, 3) * x1) + (3 * t * Math.pow(1 - t, 2) * x2)
+                    + (3 * Math.pow(t, 2) * (1 - t) * x3) + (Math.pow(t, 3) * x4);
+            double y = (Math.pow(1 - t, 3) * y1) + (3 * t * Math.pow(1 - t, 2) * y2)
+                    + (3 * Math.pow(t, 2) * (1 - t) * y3) + (Math.pow(t, 3) * y4);
+            plot(g, (int) x, (int) y);
+        }
+    }
+
+    public BufferedImage floodFill(BufferedImage m, int x, int y,
+            Color target_colour, Color replacement_colour) {
+        Graphics2D g2 = m.createGraphics();
+        Queue<Point> q = new LinkedList<>();
+
+        if (m.getRGB(x, y) == target_colour.getRGB()) {
+            g2.setColor(replacement_colour);
+            plot(g2, x, y, 1);
+            q.add(new Point(x, y));
+        }
+
+        while (!q.isEmpty()) {
+            Point p = q.poll();
+
+            if (p.y < HEIGHT && m.getRGB(p.x, p.y + 1) == target_colour.getRGB()) {
+                g2.setColor(replacement_colour);
+                plot(g2, p.x, p.y + 1, 1);
+                q.add(new Point(p.x, p.y + 1));
+            }
+            if (p.y > 0 && m.getRGB(p.x, p.y - 1) == target_colour.getRGB()) {
+                g2.setColor(replacement_colour);
+                plot(g2, p.x, p.y - 1, 1);
+                q.add(new Point(p.x, p.y - 1));
+            }
+            if (p.x < WIDTH && m.getRGB(p.x + 1, p.y) == target_colour.getRGB()) {
+                g2.setColor(replacement_colour);
+                plot(g2, p.x + 1, p.y, 1);
+                q.add(new Point(p.x + 1, p.y));
+            }
+            if (p.y > 0 && m.getRGB(p.x - 1, p.y) == target_colour.getRGB()) {
+                g2.setColor(replacement_colour);
+                plot(g2, p.x - 1, p.y, 1);
+                q.add(new Point(p.x - 1, p.y));
+            }
+        }
+        return m;
     }
 
     public void drawArc(Graphics g, int x, int y, int width, int height, int sttang, int arcang) {
