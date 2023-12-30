360a31632f06ed7a3dc1441e73d4129b225ad419
SystemCallBeam <tkhaeamadan@gmail.com>
2023-12-31 04:33:00 +0700

    add commponent and fix something

--- MyPicture.java
+++ MyPicture.java
@@ -34,31 +34,118 @@ public class MyPicture extends JPanel {
         g2.fillRect(0, 0, WIDTH, HEIGHT);
 
         g2.setColor(Color.BLACK);
-        line(g2, 0, 0, 0, HEIGHT);
-        line(g2, 0, 0, WIDTH, 0);
-        line(g2, HEIGHT - 1, 0, WIDTH - 1, HEIGHT);
-        line(g2, 0, HEIGHT - 1, WIDTH, HEIGHT - 1);
+        line(g2, 0, 0, 0, HEIGHT, 1);
+        line(g2, 0, 0, WIDTH, 0, 1);
+        line(g2, HEIGHT - 1, 0, WIDTH - 1, HEIGHT, 1);
+        line(g2, 0, HEIGHT - 1, WIDTH, HEIGHT - 1, 1);
 
         int x[] = { 590, 250, 1, 450, 299, 299, 448, 48, 150, 598 };
         int y[] = { 200, 250, 1, 150, 299, 450, 448, 330, 510, 598 };
 
-        gradient = generateGradient(Color.BLACK, new Color(173, 216, 230), 15);
+        gradient = generateGradient(Color.BLACK, new Color(173, 216, 230), 380);
 
-        // Print the generated colors
+        int i = 0;
+        int size = 1;
+
+        { // sky and sea
+          // night-sky
+            for (Color color : gradient) {
+                g2.setColor(color);
+                line(g2, 0, 0 + (int) (size * i), 300, 0 + (int) (size * i), (int) size);
+                i++;
+            }
+
+            // day-sky
+            i = 0;
+            gradient = generateGradient(new Color(5, 130, 200), new Color(220, 220, 255), 380);
+            for (Color color : gradient) {
+                g2.setColor(color);
+                line(g2, 300, 0 + (int) (size * i), 600, 0 + (int) (size * i), (int) size);
+                i++;
+            }
+
+            // day-sea
+            i = 380;
+            gradient = generateGradient(new Color(3, 80, 124), new Color(202, 240, 248), 130);
+            for (Color color : gradient) {
+                g2.setColor(color);
+                line(g2, 300, 0 + (int) (size * i), 600, 0 + (int) (size * i), (int) size);
+                i++;
+            }
+
+            // night-sea
+            i = 380;
+            gradient = generateGradient(new Color(5, 50, 80), new Color(20, 120, 168), 100);
+            for (Color color : gradient) {
+                g2.setColor(color);
+                line(g2, 0, 0 + (int) (size * i), 300, 0 + (int) (size * i), (int) size);
+                i++;
+            }
+        }
+
+        // random-star
+        i = 0;
+        size = 3;
+        gradient = generateGradient(new Color(255, 255, 255), new Color(200, 200, 200), size);
         for (Color color : gradient) {
-            System.out.println("RGB: " + color.getRed() + ", " + color.getGreen() + ", " + color.getBlue());
+            g2.setColor(color);
+            star(g2, 250, 40, i);
+            { // drago
+                star(g2, 450, 100, i);
+                star(g2, 415, 130, i);
+                star(g2, 373, 200, i);
+                star(g2, 341, 253, i);
+                star(g2, 310, 257, i);
+                star(g2, 293, 250, i);
+                star(g2, 260, 230, i);
+                star(g2, 225, 180, i);
+                star(g2, 150, 175, i);
+                star(g2, 155, 197, i);
+                star(g2, 175, 270, i);
+                star(g2, 200, 275, i);
+                star(g2, 202, 294, i);
+                star(g2, 171, 298, i);
+            }
+            i++;
+        }
+
+        // star-link
+        {
+            line(g2, 450, 100, 415, 130, 1);
+            line(g2, 415, 130, 373, 200, 1);
+            line(g2, 373, 200, 341, 253, 1);
+            line(g2, 341, 253, 310, 257, 1);
+            line(g2, 310, 257, 293, 250, 1);
+            line(g2, 293, 250, 260, 230, 1);
+            line(g2, 260, 230, 225, 180, 1);
+            line(g2, 225, 180, 150, 175, 1);
+            line(g2, 150, 175, 155, 197, 1);
+            line(g2, 155, 197, 175, 270, 1);
+            line(g2, 175, 270, 200, 275, 1);
+            line(g2, 200, 275, 202, 294, 1);
+            line(g2, 202, 294, 171, 298, 1);
+            line(g2, 171, 298, 175, 270, 1);
         }
 
         g2.setColor(Color.PINK);
-        heart(g2, 100, 100, 10);
+
+        cloud(g2, 100, 200, 14);
 
         // circle(g2, 100, 310, 40);
-        midpointCircle(g2, 80, 70, 40);
+        midpointCircle(g2, 80, 70, 34);
+        buffer = floodFillAll(buffer, 80, 70, Color.pink, Color.pink);
 
-        star(g2, 430, 130, 7);
+        // line(g2, 0, 380, 600, 380, 1);
+        // line(g2, 300, 0, 300, 600, 1);
 
-        line(g2, 0, 370, 600, 370);
-        line(g2, 300, 0, 300, 600);
+        bird(g2, 270, 300, 9);
+        bird(g2, 280, 314, 9);
+        bird(g2, 255, 333, 9);
+
+        wave1(g2, 120, 390, 6);
+        wave2(g2, 80, 390, 3);
+        wave2(g2, 80, 395, 3);
+        wave2(g2, 84, 401, 6);
 
         curve(g2, 0, 550, 70, 510, 170, 510, 200, 535);
         // curve(g2, 102, 500, 161, 510, 170, 510, 200, 535);
@@ -93,6 +180,27 @@ public class MyPicture extends JPanel {
         }
 
         return gradient;
+    }
+
+    public void wave1(Graphics g, int x, int y, int size) {
+        curve(g, x-2*size, y, x-1*size, y-1*size, x+1*size, y+1*size, x+2*size, y);
+    }
+
+    public void wave2(Graphics g, int x, int y, int size) {
+        curve(g, x-4*size, y, x-1*size, y-1*size, x+1*size, y-1*size, x+4*size, y);
+    }
+
+    public void bird(Graphics g, int x, int y, int size) {
+        curve(g, x, y, x, y-1*size, x-1*size, y-1*size, x-1*size, y);
+        curve(g, x, y, x, y-1*size, x+1*size, y-1*size, x+1*size, y);
+    }
+
+    public void cloud(Graphics g, int x, int y, int size) {
+        curve(g, x - 3 * size, y + 1 * size, x - 6 * size, y + 1 * size, x - 6 * size, y + 2 * size, x, y + 2 * size);
+        curve(g, x + 3 * size, y + 1 * size, x + 6 * size, y + 1 * size, x + 6 * size, y + 2 * size, x, y + 2 * size);
+        curve(g, x - 3 * size, y + 1 * size, x - 4 * size, y, x - 3 * size, y - 1 * size, x - 2 * size, y);
+        curve(g, x + 3 * size, y + 1 * size, x + 4 * size, y, x + 3 * size, y - 1 * size, x + 2 * size, y);
+        curve(g, x - 2 * size, y, x - 3 * size, y - 2 * size, x + 3 * size, y - 2 * size, x + 2 * size, y);
     }
 
     public void star(Graphics g, int x, int y, int size) {
@@ -190,7 +298,7 @@ public class MyPicture extends JPanel {
     }
 
     public void bezier(Graphics g, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        float n = 2000;
+        float n = 1500;
         float t = 0;
         for (; t < 1; t += (1 / n)) {
             double x = (Math.pow(1 - t, 3) * x1) + (3 * t * Math.pow(1 - t, 2) * x2)
@@ -199,6 +307,44 @@ public class MyPicture extends JPanel {
                     + (3 * Math.pow(t, 2) * (1 - t) * y3) + (Math.pow(t, 3) * y4);
             plot(g, (int) x, (int) y, 1);
         }
+    }
+
+    public BufferedImage floodFillAll(BufferedImage m, int x, int y,
+            Color target_colour, Color replacement_colour) {
+        Graphics2D g2 = m.createGraphics();
+        Queue<Point> q = new LinkedList<>();
+
+        if (m.getRGB(x, y) != target_colour.getRGB()) {
+            g2.setColor(replacement_colour);
+            plot(g2, x, y, 1);
+            q.add(new Point(x, y));
+        }
+
+        while (!q.isEmpty()) {
+            Point p = q.poll();
+
+            if (p.y < 600 && m.getRGB(p.x, p.y + 1) != target_colour.getRGB()) {
+                g2.setColor(replacement_colour);
+                plot(g2, p.x, p.y + 1, 1);
+                q.add(new Point(p.x, p.y + 1));
+            }
+            if (p.y > 0 && m.getRGB(p.x, p.y - 1) != target_colour.getRGB()) {
+                g2.setColor(replacement_colour);
+                plot(g2, p.x, p.y - 1, 1);
+                q.add(new Point(p.x, p.y - 1));
+            }
+            if (p.x < 600 && m.getRGB(p.x + 1, p.y) != target_colour.getRGB()) {
+                g2.setColor(replacement_colour);
+                plot(g2, p.x + 1, p.y, 1);
+                q.add(new Point(p.x + 1, p.y));
+            }
+            if (p.y > 0 && m.getRGB(p.x - 1, p.y) != target_colour.getRGB()) {
+                g2.setColor(replacement_colour);
+                plot(g2, p.x - 1, p.y, 1);
+                q.add(new Point(p.x - 1, p.y));
+            }
+        }
+        return m;
     }
 
     public BufferedImage floodFill(BufferedImage m, int x, int y,
@@ -243,10 +389,10 @@ public class MyPicture extends JPanel {
         plot(g, x, y);
     }
 
-    public void line(Graphics g, int x1, int y1, int x2, int y2) {
+    public void line(Graphics g, int x1, int y1, int x2, int y2, int size) {
         // g.drawLine(x1, y1, x2, y2);
         // dda(g, x1, y1, x2, y2);
-        bresenham(g, x1, y1, x2, y2);
+        bresenham(g, x1, y1, x2, y2, size);
     }
 
     public void plot(Graphics g, int x, int y) {
@@ -273,7 +419,7 @@ public class MyPicture extends JPanel {
         }
     }
 
-    public void bresenham(Graphics g, int x1, int y1, int x2, int y2) {
+    public void bresenham(Graphics g, int x1, int y1, int x2, int y2, int size) {
         int dx = Math.abs(x2 - x1);
         int dy = Math.abs(y2 - y1);
 
@@ -292,7 +438,7 @@ public class MyPicture extends JPanel {
         int y = y1;
 
         for (int i = 0; i < dx; i++) {
-            plot(g, x, y);
+            plot(g, x, y, size);
 
             if (d >= 0) {
                 if (isSwap)
