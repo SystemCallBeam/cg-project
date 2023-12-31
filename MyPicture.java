5b8bd54a6caffcb9b1c064be89541e23b6b98e56
SystemCallBeam <tkhaeamadan@gmail.com>
2024-01-01 06:59:04 +0700

    update picture

--- MyPicture.java
+++ MyPicture.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -10,14 +11,21 @@ public class MyPicture extends JPanel {
     public static final int WIDTH = 600, HEIGHT = 600;
 
     public static Random rand = new Random();
-    public static Color randomColor[] = new Color[10];
+    // public static Color randomColor[] = new Color[10];
     public static Color[] gradient;
+    public static int r[][] = new int[WIDTH][HEIGHT];
 
     public static void main(String[] args) {
         MyPicture g = new MyPicture();
         JFrame f = new JFrame("My Picture");
-        for (int i = 0; i < 10; i++) {
-            randomColor[i] = new Color(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256));
+        // for (int i = 0; i < 10; i++) {
+        // randomColor[i] = new Color(rand.nextInt(256), rand.nextInt(256),
+        // rand.nextInt(256));
+        // }
+        for (int i = 0; i < r.length; i++) {
+            for (int j = 0; j < r[0].length; j++) {
+                r[i][j] = rand.nextInt(25);
+            }
         }
         f.setSize(WIDTH, HEIGHT);
         f.add(g);
@@ -39,13 +47,10 @@ public class MyPicture extends JPanel {
         line(g2, HEIGHT - 1, 0, WIDTH - 1, HEIGHT, 1);
         line(g2, 0, HEIGHT - 1, WIDTH, HEIGHT - 1, 1);
 
-        int x[] = { 590, 250, 1, 450, 299, 299, 448, 48, 150, 598 };
-        int y[] = { 200, 250, 1, 150, 299, 450, 448, 330, 510, 598 };
-
         gradient = generateGradient(Color.BLACK, new Color(173, 216, 230), 380);
 
-        int i = 0;
-        int size = 1;
+        int x, y, temp;
+        int i = 0, size = 1;
 
         { // sky and sea
           // night-sky
@@ -89,7 +94,23 @@ public class MyPicture extends JPanel {
         gradient = generateGradient(new Color(255, 255, 255), new Color(200, 200, 200), size);
         for (Color color : gradient) {
             g2.setColor(color);
-            star(g2, 250, 40, i);
+            { // star
+                star(g2, 250, 40, i);
+                star(g2, 190, 24, i);
+                star(g2, 130, 48, i);
+                star(g2, 220, 50, i);
+                star(g2, 230, 80, i);
+                star(g2, 20, 40, i);
+                star(g2, 30, 90, i);
+                star(g2, 150, 130, i);
+                star(g2, 50, 150, i);
+                star(g2, 180, 110, i);
+                star(g2, 330, 80, i);
+                star(g2, 440, 40, i);
+                star(g2, 300, 90, i);
+                star(g2, 550, 130, i);
+            }
+
             { // drago
                 star(g2, 450, 100, i);
                 star(g2, 415, 130, i);
@@ -127,41 +148,149 @@ public class MyPicture extends JPanel {
             line(g2, 171, 298, 175, 270, 1);
         }
 
-        g2.setColor(Color.PINK);
+        Color colorA = Color.white;
+        { // cloud
+            g2.setColor(colorA);
+            cloud(g2, 250, 100, 10);
+            cloud(g2, 100, 200, 14);
+            cloud(g2, 430, 290, 14);
+            floodFillAll(buffer, 250, 100, colorA);
+            floodFillAll(buffer, 100, 200, colorA);
+            floodFillAll(buffer, 430, 290, colorA);
 
-        cloud(g2, 100, 200, 14);
+            colorA = new Color(230, 230, 230);
+            g2.setColor(colorA);
+            cloud(g2, 250, 104, 7);
+            cloud(g2, 100, 206, 10);
+            cloud(g2, 430, 299, 10);
+            floodFillAll(buffer, 250, 100, colorA);
+            floodFillAll(buffer, 100, 200, colorA);
+            floodFillAll(buffer, 430, 299, colorA);
+        }
 
-        // circle(g2, 100, 310, 40);
+        colorA = new Color(255, 250, 200);
+        g2.setColor(colorA);
         midpointCircle(g2, 80, 70, 34);
-        buffer = floodFillAll(buffer, 80, 70, Color.pink, Color.pink);
+        buffer = floodFillAll(buffer, 80, 70, colorA);
+        i = 0;
+        size = 8;
+        gradient = generateGradient(new Color(255, 255, 255), new Color(200, 200, 200), size);
+        for (Color color : gradient) {
+            g2.setColor(color);
+            midpointCircle(g2, 80, 70, 34 + i);
+            i++;
+        }
 
         // line(g2, 0, 380, 600, 380, 1);
         // line(g2, 300, 0, 300, 600, 1);
 
+        { // wave
+            g2.setColor(new Color(188, 239, 248));
+            wave2(g2, 80, 390, 3, 2);
+            wave2(g2, 84, 401, 3, 2);
+            wave2(g2, 283, 399, 3, 2);
+            wave2(g2, 104, 421, 5, 2);
+            wave2(g2, 174, 431, 5, 2);
+            wave2(g2, 84, 441, 5, 2);
+            wave2(g2, 224, 391, 3, 2);
+            wave2(g2, 40, 428, 6, 2);
+            wave2(g2, 254, 441, 6, 2);
+            line(g2, 20, 395, 40, 395, 2);
+            line(g2, 140, 397, 160, 397, 2);
+            line(g2, 120, 410, 150, 410, 2);
+            line(g2, 200, 405, 230, 405, 2);
+
+            x = 290;
+            wave2(g2, 80 + x, 390, 3, 3);
+            wave2(g2, 84 + x, 401, 3, 3);
+            wave2(g2, 283 + x, 399, 3, 3);
+            wave2(g2, 104 + x, 421, 5, 3);
+            wave2(g2, 174 + x, 431, 5, 3);
+            wave2(g2, 84 + x, 441, 5, 3);
+            wave2(g2, 224 + x, 391, 3, 3);
+            wave2(g2, 40 + x, 428, 6, 3);
+            wave2(g2, 254 + x, 441, 6, 3);
+            line(g2, 20 + x, 395, 40 + x, 395, 2);
+            line(g2, 140 + x, 397, 160 + x, 397, 2);
+            line(g2, 120 + x, 410, 150 + x, 410, 2);
+            line(g2, 200 + x, 405, 230 + x, 405, 2);
+        }
+
+        g2.setColor(Color.black);
         bird(g2, 270, 300, 9);
         bird(g2, 280, 314, 9);
         bird(g2, 255, 333, 9);
 
-        wave1(g2, 120, 390, 6);
-        wave2(g2, 80, 390, 3);
-        wave2(g2, 80, 395, 3);
-        wave2(g2, 84, 401, 6);
+        { // ground
+            int ty = 1;
+            Color green_1 = new Color(50, 87, 48);
+            g2.setColor(green_1);
+            curve(g2, 0, 450, 180, 430, 250, 450, 380, 510);
+            line(g2, 0, 450, 0, 550, 1);
 
-        curve(g2, 0, 550, 70, 510, 170, 510, 200, 535);
-        // curve(g2, 102, 500, 161, 510, 170, 510, 200, 535);
-        curve(g2, 200, 535, 230, 560, 310, 560, 380, 510);
-        curve(g2, 380, 510, 450, 465, 550, 465, 600, 475);
+            Color green_2 = new Color(80, 120, 45);
+            g2.setColor(green_2);
+            line(g2, 0, 551, 0, 600, 1);
+            line(g2, 0, 600 - 1, 600, 600 - 1, 1);
+            line(g2, 600 - 1, 475, 600 - 1, 600, 2);
+            curve(g2, 0, 550, 70, 510, 170, 510, 200, 535);
+            curve(g2, 200, 535, 230, 560, 310, 560, 380, 510);
+            curve(g2, 380, 510, 450, 465, 550, 465, 600, 475);
 
-        int ty = 10;
-        curve(g2, 98, 510 + ty, 107, 510 + ty, 113, 492 + ty, 116, 492 + ty);
-        curve(g2, 116, 492 + ty, 119, 492 + ty, 122, 501 + ty, 125, 501 + ty);
-        curve(g2, 125, 501 + ty, 128, 501 + ty, 131, 492 + ty, 134, 492 + ty);
-        curve(g2, 134, 492 + ty, 137, 492 + ty, 143, 510 + ty, 152, 510 + ty);
+            ty = 10;
+            curve(g2, 98, 510 + ty, 107, 510 + ty, 113, 492 + ty, 116, 492 + ty);
+            curve(g2, 116, 492 + ty, 119, 492 + ty, 122, 501 + ty, 125, 501 + ty);
+            curve(g2, 125, 501 + ty, 128, 501 + ty, 131, 492 + ty, 134, 492 + ty);
+            curve(g2, 134, 492 + ty, 137, 492 + ty, 143, 510 + ty, 152, 510 + ty);
 
-        curve(g2, 0, 450, 180, 430, 250, 450, 380, 510);
+            floodFillAll(buffer, 300, 598, green_2);
+            floodFill(buffer, 115, 505, Color.white, green_2);
+            plot(g2, 115, 506, 1);
 
-        // buffer = floodFill(buffer, 100, 50, Color.white, Color.red);
-        // buffer = floodFill(buffer, 500, 500, Color.white, Color.blue);
+            g2.setColor(green_1);
+            floodFill(buffer, 200, 500, Color.white, green_1);
+            floodFillAll(buffer, 200, 455, green_1);
+        }
+
+        g2.setColor(Color.black);
+        flower(buffer, g2, 200, 455, 5);
+        flower(buffer, g2, 174, 480, 5);
+        flower(buffer, g2, 124, 492, 4);
+        flower(buffer, g2, 134, 472, 5);
+        flower(buffer, g2, 84, 482, 4);
+        flower(buffer, g2, 264, 492, 6);
+        flower(buffer, g2, 230, 499, 4);
+        flower(buffer, g2, 291, 512, 5);
+
+        x = 0;
+        y = 14;
+        size = 6;
+        g2.setColor(Color.black);
+        midpointCircle(g2, 116, 515, 3);
+        midpointCircle(g2, 133, 515, 3);
+        floodFillAll(buffer, 116, 515, Color.black);
+        floodFillAll(buffer, 133, 515, Color.black);
+        curve(g2, 125 + x, 510 + y, 125 + x, 510 + y + 1 * size, 125 + x - 1 * size, 510 + y + 1 * size,
+                125 + x - 1 * size, 510 + y);
+        curve(g2, 125 + x, 510 + y, 125 + x, 510 + y + 1 * size, 125 + x + 1 * size, 510 + y + 1 * size,
+                125 + x + 1 * size, 510 + y);
+
+        temp = 5;
+        for (int j = 0; j < 600 / temp; j++) {
+            for (int k = 0; k < 300 / temp; k++) {
+                colorA = new Color(buffer.getRGB(300 + temp * k, 0 + temp * j) - r[j][k]);
+                g2.setColor(colorA);
+                plot(g2, 300 + temp * k, 0 + temp * j, temp);
+            }
+        }
+
+        for (int j = 0; j < 600; j++) {
+            for (int k = 0; k < 600; k++) {
+                colorA = new Color(buffer.getRGB(j, k) - r[j][k]);
+                g2.setColor(colorA);
+                // plot(g2, j, k);
+            }
+        }
 
         g.drawImage(buffer, 0, 0, null);
 
@@ -182,17 +311,43 @@ public class MyPicture extends JPanel {
         return gradient;
     }
 
-    public void wave1(Graphics g, int x, int y, int size) {
-        curve(g, x-2*size, y, x-1*size, y-1*size, x+1*size, y+1*size, x+2*size, y);
+    public void flower(BufferedImage m, Graphics g, int x, int y, int size) {
+        Color c;
+        g.setColor(Color.black);
+        line(g, x, y, x, y + 4 * size, 1);
+
+        c = Color.white;
+        g.setColor(c);
+        curve(g, x - 1 * size, y - 1 * size, x - 1 * size, y - 2 * size, x + 1 * size, y - 2 * size, x + 1 * size,
+                y - 1 * size);
+        curve(g, x - 1 * size, y + 1 * size, x - 1 * size, y + 2 * size, x + 1 * size, y + 2 * size, x + 1 * size,
+                y + 1 * size);
+        curve(g, x - 1 * size, y - 1 * size, x - 2 * size, y - 1 * size, x - 2 * size, y + 1 * size, x - 1 * size,
+                y + 1 * size);
+        curve(g, x + 1 * size, y - 1 * size, x + 2 * size, y - 1 * size, x + 2 * size, y + 1 * size, x + 1 * size,
+                y + 1 * size);
+        floodFillAll(m, x + 1 * size + 1, y, c);
+        floodFillAll(m, x - 1 * size - 1, y, c);
+        floodFillAll(m, x, y + 1 * size + 1, c);
+        floodFillAll(m, x, y - 1 * size - 1, c);
+        c = new Color(255, 226, 0);
+        // c = Color.yellow;
+        g.setColor(c);
+        midpointCircle(g, x, y, size);
+        floodFillAll(m, x, y, c);
+    }
+
+    public void wave2(Graphics g, int x, int y, int size, int scale) {
+        curve(g, x - 4 * size, y, x - 1 * size, y - 1 * size, x + 1 * size, y - 1 * size, x + 4 * size, y, scale);
     }
 
     public void wave2(Graphics g, int x, int y, int size) {
-        curve(g, x-4*size, y, x-1*size, y-1*size, x+1*size, y-1*size, x+4*size, y);
+        wave2(g, x, y, size, 1);
     }
 
     public void bird(Graphics g, int x, int y, int size) {
-        curve(g, x, y, x, y-1*size, x-1*size, y-1*size, x-1*size, y);
-        curve(g, x, y, x, y-1*size, x+1*size, y-1*size, x+1*size, y);
+        curve(g, x, y, x, y - 1 * size, x - 1 * size, y - 1 * size, x - 1 * size, y);
+        curve(g, x, y, x, y - 1 * size, x + 1 * size, y - 1 * size, x + 1 * size, y);
     }
 
     public void cloud(Graphics g, int x, int y, int size) {
@@ -293,29 +448,37 @@ public class MyPicture extends JPanel {
         }
     }
 
+    public void curve(Graphics g, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int size) {
+        bezier(g, x1, y1, x2, y2, x3, y3, x4, y4, size);
+    }
+
     public void curve(Graphics g, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
         bezier(g, x1, y1, x2, y2, x3, y3, x4, y4);
     }
 
     public void bezier(Graphics g, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        float n = 1500;
+        bezier(g, x1, y1, x2, y2, x3, y3, x4, y4, 1);
+    }
+
+    public void bezier(Graphics g, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int size) {
+        float n = 1200;
         float t = 0;
         for (; t < 1; t += (1 / n)) {
             double x = (Math.pow(1 - t, 3) * x1) + (3 * t * Math.pow(1 - t, 2) * x2)
                     + (3 * Math.pow(t, 2) * (1 - t) * x3) + (Math.pow(t, 3) * x4);
             double y = (Math.pow(1 - t, 3) * y1) + (3 * t * Math.pow(1 - t, 2) * y2)
                     + (3 * Math.pow(t, 2) * (1 - t) * y3) + (Math.pow(t, 3) * y4);
-            plot(g, (int) x, (int) y, 1);
+            plot(g, (int) x, (int) y, size);
         }
     }
 
     public BufferedImage floodFillAll(BufferedImage m, int x, int y,
-            Color target_colour, Color replacement_colour) {
+            Color target_colour) {
         Graphics2D g2 = m.createGraphics();
         Queue<Point> q = new LinkedList<>();
 
         if (m.getRGB(x, y) != target_colour.getRGB()) {
-            g2.setColor(replacement_colour);
+            g2.setColor(target_colour);
             plot(g2, x, y, 1);
             q.add(new Point(x, y));
         }
@@ -324,22 +487,22 @@ public class MyPicture extends JPanel {
             Point p = q.poll();
 
             if (p.y < 600 && m.getRGB(p.x, p.y + 1) != target_colour.getRGB()) {
-                g2.setColor(replacement_colour);
+                g2.setColor(target_colour);
                 plot(g2, p.x, p.y + 1, 1);
                 q.add(new Point(p.x, p.y + 1));
             }
             if (p.y > 0 && m.getRGB(p.x, p.y - 1) != target_colour.getRGB()) {
-                g2.setColor(replacement_colour);
+                g2.setColor(target_colour);
                 plot(g2, p.x, p.y - 1, 1);
                 q.add(new Point(p.x, p.y - 1));
             }
             if (p.x < 600 && m.getRGB(p.x + 1, p.y) != target_colour.getRGB()) {
-                g2.setColor(replacement_colour);
+                g2.setColor(target_colour);
                 plot(g2, p.x + 1, p.y, 1);
                 q.add(new Point(p.x + 1, p.y));
             }
             if (p.y > 0 && m.getRGB(p.x - 1, p.y) != target_colour.getRGB()) {
-                g2.setColor(replacement_colour);
+                g2.setColor(target_colour);
                 plot(g2, p.x - 1, p.y, 1);
                 q.add(new Point(p.x - 1, p.y));
             }
